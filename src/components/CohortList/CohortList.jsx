4fd254561067c6116ad56d8d6dd0a7b1abc9e685
KatWine <kwinegardner@pursuit.org>
2024-05-07 16:33:22 -0400

    final push on dashboard

--- src/components/CohortList/CohortList.jsx
+++ src/components/CohortList/CohortList.jsx
@@ -1,20 +1,26 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
+// CohortList.jsx
+import React from 'react';
 import CohortListItem from './CohortListItem';
 import './cohortlist.scss';
 
-
 const CohortList = ({ setCohort, setCurrentCohort }) => {
-  const cohorts = ['All Students','Winter 2026', 'Fall 2026', 'Summer 2026', 'Spring 2026', 'Winter 2025', 'Fall 2025', 'Summer 2025', 'Spring 2025'];
+  const cohorts = ['All Students', 'Winter 2026', 'Fall 2026', 'Summer 2026', 'Spring 2026', 'Winter 2025', 'Fall 2025', 'Summer 2025', 'Spring 2025'];
 
   return (
-    <aside className="cohort-list">
-        <h3 className='cohort-list__header'> Choose a Class by Start Date</h3>
-        <ul> 
-          {cohorts.map((cohort, index) => (<CohortListItem setCurrentCohort={setCurrentCohort} setCohort={setCohort} cohort={cohort} key={index} />))}
-        </ul>
+    <aside className='cohort-list'>
+      <h3 className='cohort-list__header'>Choose a Class by Start Date</h3>
+      <ul>
+        {cohorts.map((cohort, index) => (
+          <CohortListItem
+            setCurrentCohort={setCurrentCohort}
+            setCohort={setCohort}
+            cohort={cohort}
+            key={index}
+          />
+        ))}
+      </ul>
     </aside>
-  )
+  );
 }
 
-export default CohortList
+export default CohortList;
--- src/components/StudentList/StudentList.jsx
+++ src/components/StudentList/StudentList.jsx
@@ -1,19 +1,20 @@
-/* eslint-disable react/prop-types */
+// StudentList.jsx
 import React from 'react';
 import './studentlist.scss';
 import StudentListItem from './StudentListItem/StudentListItem';
 
 const StudentList = ({ cohort, currentCohort }) => {
   return (
-    <main className="student-list">
-        <h3>{cohort}</h3>
-        <p> Total Students: <span>{currentCohort.length}</span></p>
-        <ul className="student-list__item">
-          {currentCohort.map((student) => (<li key={student.id}><StudentListItem student={student} /></li>))}
-        </ul>
-        
+    <main className='student-list'>
+      <h3>{cohort}</h3>
+      <p>Total Students: <span>{currentCohort.length}</span></p>
+      <ul className='student-list__item'>
+        {currentCohort.map((student) => (
+          <li key={student.id}><StudentListItem student={student} /></li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
 
-export default StudentList
+export default StudentList;
